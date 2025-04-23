82b09a634d4df951296d8968412ad8842831edc1
edson,pereira15 <edson.pereira15@fatec.sp.gov.br>
2025-04-23 12:29:46 -0300

    Criação EntitieConfiguration

---
+++ BioSyncBackEnd/BioSync.Infra.Data/EntitiesConfiguration/AgendamentoConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using BioSync.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BioSync.Infra.Data.EntitiesConfiguration
+{
+    public class AgendamentoConfiguration
+    {
+        public void Configure(EntityTypeBuilder<Agendamento> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(p => p.Data);
+            builder.Property(p => p.HoraInicioDisponivel);
+            builder.Property(p => p.HoraInicioDisponivel);
+            builder.Property(p => p.Status);
+            builder.Property(p => p.PesoEstimadoKg);
+            builder.Property(p => p.FotoResiduos);
+            builder.Property(p => p.Observacoes);
+
+            builder.HasOne(e => e.Usuario).WithMany(e => e.Agendamentos).HasForeignKey(e => e.UsuarioId);
+           
+
+        }
+    }
+}
---
+++ BioSyncBackEnd/BioSync.Infra.Data/EntitiesConfiguration/PessoaConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using BioSync.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace BioSync.Infra.Data.EntitiesConfiguration
+{
+    public class PessoaConfiguration
+    {
+        public void Configure(EntityTypeBuilder<Pessoa> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(p => p.Nome);
+            builder.Property(p => p.CPF);
+            builder.Property(p => p.Telefone);
+            builder.Property(p => p.Email);
+
+        }
+    }
+}
